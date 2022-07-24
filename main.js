76cb05e9752f1e3c3fb1478e3699969f54a06d08
davipucodigo <davi.santvieira@gmail.com>
2022-07-24 20:01:07 +0000

    s

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
 const canvas = document.getElementById("canvas")
+canvas.width = 512
+canvas.height = 512
+canvas.style.border = "1px solid black"
 const c = canvas.getContext("2d")
 
 //sprite
 var bloco1 = new Image()
 bloco1.src = "sprites/spritesheet_vanhell_game.png"
 
-
-
 // map
 const map = [
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [1,1,1,1,1,1,1,1,1,1,1,1]
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1]
 ]
 
-for (var y = 0; y < map.length; y++) {
-    for (var x = 0; x < map[y].length; x++) {
-        console.log(map[y][x])
-    }
-}
-
-
+var scale = 4
 function Update(){
-    bloco1.addEventListener("load", () => {
-        c.drawImage(bloco1, 30, 30)
-    })
-    //animate
+     // Uma gambiarra do carambulango
+     for (var y = 0; y < map.length; y++) {
+        for (var x = 0; x < map[y].length; x++) {
+            if (map[y][x] == 1) {
+                c.drawImage( bloco1, 10*16,3*16,16,16, x*16*scale, y*16*scale, 16*scale, 16*scale)
+            }
+        }
+    }
+    //loop area
     requestAnimationFrame(Update)
 }
 Update()
+
+c.imageSmoothingEnabled = false;
