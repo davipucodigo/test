0d5e360ace8867ff2c5d02ad706ffd5388617e7e
davipucodigo <davi.santvieira@gmail.com>
2022-07-28 17:18:27 +0000

    l

--- main.js
+++ main.js
@@ -3,10 +3,36 @@ canvas.width = 512
 canvas.height = 512
 canvas.style.border = "1px solid black"
 const c = canvas.getContext("2d")
+// Class
+class Sprite {
+    constructor (img,wid,hei,widcut,heicut,xcut,ycut,  ) {
+        this.img = img
+        this.scale = 16
+        this.size = 4
+        this.wid = wid
+        this.hei = hei
+        this.x
+        this.y
+        this.widcut = widcut
+        this.heicut = heicut
+        this.xcut = xcut
+        this.ycut = ycut
+    }
+    Draw (x,y) {
+        this.x = x
+        this.y = y
+        c.drawImage(this.img, this.xcut*this.scale, this.ycut*this.scale, this.scale, this.scale, this.x*this.scale*this.size, this.y*this.scale*this.size, this.wid*this.size, this.hei*this.size)
+    }
+}
+
 
 //sprite
-var bloco1 = new Image()
-bloco1.src = "sprites/spritesheet_vanhell_game.png"
+var spritesheet = new Image()
+spritesheet.src = "sprites/spritesheet_vanhell_game.png"
+
+var block1 = new Sprite(spritesheet,16,16,16,16,10,3)
+
+var FireBoll = new Sprite(spritesheet,16,16,16,16,4,0)
 
 // map
 const map = [
@@ -14,24 +40,27 @@ const map = [
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
+    [0,0,0,2,0,0,0,0],
     [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0],
     [1,1,1,1,1,1,1,1]
 ]
 
-var scale = 4
+
 function Update(){
     c.fillStyle = 'black';
     c.fillRect(0,0,512,521)
-     // Uma gambiarra do carambulango
-     for (var y = 0; y < map.length; y++) {
-        for (var x = 0; x < map[y].length; x++) {
+
+     // Map Render
+     for (let y = 0; y < map.length; y++) {
+        for (let x = 0; x < map[y].length; x++) {
             if (map[y][x] == 1) {
-                c.drawImage( bloco1, 10*16,3*16,16,16, x*16*scale, y*16*scale, 16*scale, 16*scale)
+                block1.Draw(x,y)
             }
-        }
-    }
+            if (map[y][x] == 2){
+                FireBoll.Draw(x,y)
+            }}}
+            
     //loop area
     requestAnimationFrame(Update)
 }
